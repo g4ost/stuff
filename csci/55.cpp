4c1047aacc55525c713947932460b43f6be50182
g4ost <37353294+g4ost@users.noreply.github.com>
2018-05-03 17:01:15 -0400

    Create 55.cpp

---
+++ csci/55.cpp
@@ -0,0 +1,16 @@
+// stanley gao
+// 5/1/2018
+// assignment 54
+
+#include <iostream>
+using namespace std;
+
+extern int a, b;
+
+int main()
+{
+  int a, b;
+  b= = a*0.621371
+  cout << b << endl;
+  return0;
+}
