366dae3808739cc40ebd36987e4090e021e9f7f2
g4ost <37353294+g4ost@users.noreply.github.com>
2018-05-03 17:55:52 -0400

    Update 55.cpp

--- csci/55.cpp
+++ csci/55.cpp
@@ -5,12 +5,13 @@
 #include <iostream>
 using namespace std;
 
-extern int a, b;
-
 int main()
 {
-  int a, b;
-  b= = a*0.621371
-  cout << b << endl;
-  return0;
-}
+  double a;
+  cout << "";
+  cin >> a
+    double b
+    b = a*0.621371
+    cout >> b
+    return0;
+}  
