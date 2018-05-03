44d2fc39cb272ef8208abc8681a241bf670ed2ba
g4ost <37353294+g4ost@users.noreply.github.com>
2018-05-03 16:47:35 -0400

    Create 54.py

---
+++ csci/54.py
@@ -0,0 +1,16 @@
+// stanley gao
+// 5/1/2018
+// assignment 54
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i;
+  for (i = 0; i < 20; i++)
+  {
+    cout << "I love Python!\n";
+  }
+  return 0;
+}
